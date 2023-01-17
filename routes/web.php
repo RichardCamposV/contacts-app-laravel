af24ac39164bb81d2524d67ecadd01c912556e22
Ricardo Campos <ricard_halo@live.com.mx>
2023-01-17 17:51:26 -0600

    Delete contact

--- routes/web.php
+++ routes/web.php
@@ -26,5 +26,6 @@ Route::get('/home', [HomeController::class, 'index'])->name('home');
 
 Route::get('/contacts/create', [ContactController::class, 'create'])->name('contacts.create');
 Route::get('/contacts/{contact}/edit', [ContactController::class, 'edit'])->name('contacts.edit');
-Route::put('/contacts/{contact}/update', [ContactController::class, 'update'])->name('contacts.update');
+Route::put('/contacts/{contact}/', [ContactController::class, 'update'])->name('contacts.update');
 Route::post('/contacts/', [ContactController::class, 'store'])->name('contacts.store');
+Route::delete('/contacts/{contact}/', [ContactController::class, 'destroy'])->name('contacts.destroy');
